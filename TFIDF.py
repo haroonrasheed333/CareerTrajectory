bb7643aab0faaa1a4c06a8f7c207269758e9e6a0
Haroon Rasheed <haroonrasheed@berkeley.edu>
2013-11-24 01:26:54 -0800

    Update Cosine Similarity code

--- TFIDF.py
+++ TFIDF.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 import string
 import numpy
 from collections import Counter
+import math
 
 stopwords = stopwords.words('english')
 punct = string.punctuation
@@ -11,18 +12,30 @@ punct = string.punctuation
 
 def compute_frequency_scores(docs):
 
-    def tf_idf(document):
-        word_tfidf = []
-        for word in unique_words:
-            word_tfidf.append(text_collection.tf_idf(word, document))
-        return word_tfidf
-
     def term_freq(document):
         freq = Counter(document)
         word_freq = []
         for word in unique_words:
-            word_freq.append(freq[word])
+            word_freq.append(freq[word]/len(document))
         return word_freq
+
+    def inverse_doc_freq(word):
+        matches = len(list(True for text in text_collection._texts if word in text))
+
+        if matches > 0:
+            idf = 1.0 + math.log(float(len(text_collection._texts)) / matches)
+        else:
+            idf = 1.0
+
+        return idf
+
+    def tf_idf(document):
+        freq = Counter(document)
+        word_tfidf = []
+        for word in unique_words:
+            tfidf = (freq[word] / len(document)) * inverse_doc_freq(word)
+            word_tfidf.append(tfidf)
+        return word_tfidf
 
     texts = []
     frequencies = dict()
@@ -36,11 +49,11 @@ def compute_frequency_scores(docs):
     text_collection = nltk.TextCollection(texts)
     unique_words = list(set(text_collection))
 
-    tfidf_vectors = [numpy.array(tf_idf(f)) for f in texts]
+    tfidf_vectors = [numpy.array(tf_idf(text)) for text in texts]
     frequencies['tf_idf'] = []
     frequencies['tf_idf'] = tfidf_vectors
 
-    freq_vectors = [numpy.array(term_freq(f)) for f in texts]
+    freq_vectors = [numpy.array(term_freq(text)) for text in texts]
     frequencies['term_freq'] = []
     frequencies['term_freq'] = freq_vectors
 
@@ -52,13 +65,14 @@ def cos_sim(v1, v2):
 
 
 def main():
-    docs = ['Div.txt', 'HN.txt']
+    docs = ['JobDesc.txt', 'HN.txt']
     frequencies = compute_frequency_scores(docs)
     tfidf_vectors = frequencies['tf_idf']
-    print cos_sim(tfidf_vectors[0], tfidf_vectors[1])
+    cosine_similarity =  cos_sim(tfidf_vectors[0], tfidf_vectors[1])
+    print cosine_similarity
 
     freq_vectors = frequencies['term_freq']
-    print cos_sim(freq_vectors[0], freq_vectors[1])
+    #print cos_sim(freq_vectors[0], freq_vectors[1])
 
 
 if __name__ == '__main__':
